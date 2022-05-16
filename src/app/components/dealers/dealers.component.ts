11e3817ff63424f16e03d7ca4b7c011e7541c9bc
dave <angelnmara@hotmail.com>
2022-05-15 20:55:57 -0500

    format file

--- src/app/components/dealers/dealers.component.ts
+++ src/app/components/dealers/dealers.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, AfterViewInit, ViewChild, ChangeDetectorRef } from '
 import { MatTableDataSource } from '@angular/material/table';
 import { Dealer } from 'src/app/core/model/dealer/dealer.model';
 import { DealerService } from 'src/app/core/services/dealer/dealer.service';
-import {MatPaginator, MatPaginatorIntl} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 
 @Component({
@@ -15,11 +15,11 @@ export class DealersComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<any>();
   displayedColumns: string[] = ['id', 'name', 'addres', 'year', 'status'];
   proveedor: Dealer = { id: "", name: "", addres: "", year: "" }
-  showList:boolean=true;
+  showList: boolean = true;
   @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
   @ViewChild(MatSort) sort: MatSort = new MatSort();
 
-  constructor(private dealerService:DealerService) { 
+  constructor(private dealerService: DealerService) {
 
   }
 
@@ -41,64 +41,64 @@ export class DealersComponent implements OnInit, AfterViewInit {
     }
   }
 
-  loadAllDealers(){
-    this.dealerService.getDealers().subscribe((resp)=>{
+  loadAllDealers() {
+    this.dealerService.getDealers().subscribe((resp) => {
       console.log(resp)
       this.dataSource.data = resp
     })
   }
 
-  getDealersById(id:string){
-    this.dealerService.getDealerById(id).subscribe((resp)=>{
+  getDealersById(id: string) {
+    this.dealerService.getDealerById(id).subscribe((resp) => {
       console.log(resp)
       this.proveedor = resp
     })
   }
 
-  editDealer(id:string){
+  editDealer(id: string) {
     console.log('editar ' + id)
-    if(id.length>0){
-      this.showList=false;      
+    if (id.length > 0) {
+      this.showList = false;
       this.getDealersById(id);
     }
   }
 
-  deleteDealer(id:string){
+  deleteDealer(id: string) {
     console.log('borrar')
-    this.dealerService.deleteDealer(id).subscribe((resp)=>{
+    this.dealerService.deleteDealer(id).subscribe((resp) => {
       console.log(resp);
       this.loadAllDealers();
     })
   }
 
-  newDealer(){
+  newDealer() {
     console.log('nuevo diler')
-    this.showList=false;
+    this.showList = false;
   }
 
-  returnToList(){
-    this.showList=true;
+  returnToList() {
+    this.showList = true;
 
   }
 
-  cleanDealer(){
-    this.proveedor.name=''
-    this.proveedor.addres=''
-    this.proveedor.year=''
+  cleanDealer() {
+    this.proveedor.name = ''
+    this.proveedor.addres = ''
+    this.proveedor.year = ''
   }
 
-  validaObligatorios(element:any){
-    var visible:boolean=false;
-    if(element.name.length>0 && element.addres.length>0 && element.year.length>0){
+  validaObligatorios(element: any) {
+    var visible: boolean = false;
+    if (element.name.length > 0 && element.addres.length > 0 && element.year.length > 0) {
       visible = true;
-    }else{
-      visible=false;
-    }  
+    } else {
+      visible = false;
+    }
     return visible;
   }
 
-  saveDealer(){
-    this.dealerService.saveDealer(this.proveedor).subscribe((resp)=>{
+  saveDealer() {
+    this.dealerService.saveDealer(this.proveedor).subscribe((resp) => {
       console.log(resp);
       this.loadAllDealers();
       this.cleanDealer();
