8a79a29140e387067a74c20fd17dcabc136a9836
dave <angelnmara@hotmail.com>
2022-05-15 20:48:54 -0500

    modifica routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DealersComponent } from './components/dealers/dealers.component';
+import { HomeComponent } from './components/home/home.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 
 const routes: Routes = [
+  {
+    path:'', component:HomeComponent
+  },
   {
     path:'proveedores', component: DealersComponent
   }, {
